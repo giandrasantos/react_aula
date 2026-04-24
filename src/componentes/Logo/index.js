ad5c4632221bb8d1e238d538824a662f45a75426
giandrasantos <giandra.santos@aluno.senai.br>
2026-04-24 16:41:44 -0300

    aula 24/04

---
+++ src/componentes/Logo/index.js
@@ -0,0 +1,23 @@
+import logo from '../../imagens/logo.svg';
+import styled from 'styled-components';
+
+const LogoContainer = styled.div`
+  display: flex;
+  font-size: 30px;
+  text-align: center;
+`
+
+const LogoImage = styled.img`
+    margin-right: 10px;
+`
+
+function Logo() {
+    return (
+        <LogoContainer>
+            <LogoImage src={logo} alt="Logo" className="logo-img"></LogoImage>
+            <p><strong>SENAI Books</strong></p>
+        </LogoContainer>
+    )
+}
+
+export default Logo
